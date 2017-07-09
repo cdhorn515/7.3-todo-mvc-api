4acd661cafbf649f21b96741ca46cd9537cbe6ce
Christina Cornell <cdhorn515@gmail.com>
2017-07-08 21:13:03 -0400

    get and post are working

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const Todos = require('./models/todos');
 
 const app = express();
 
-var todos = mongoose.model('Todos', todoSchema);
+// var todos = mongoose.model('Todos', todosSchema);
 // module.exports = Todo;
+mongoose.Promise = require('bluebird');
+//create db named cdcdb
+mongoose.connect('mongodb://localhost:27017/cdcdb');
 
 app.use('/static', express.static('static'));
 app.use(bodyParser.json());
-mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost:27017/cdcdb');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/static/index.html");
@@ -23,36 +25,45 @@ app.get('/', function (req, res) {
 // create api endpoint
 // GET /api/todos/ - return a JSON array of todo items
 app.get('/api/todos', function (req, res) {
-  res.json(todos);
+  Todos.find({}).then(function(results){
+    res.json(results);
+  });
 });
 
 // POST /api/todos/ - post a JSON representation of a todo and have it saved. Returns the saved todo item in JSON.
 app.post('/api/todos', function (req, res) {
-  res.json(todos);
+  var todo = new Todos ({
+    title: req.body.title,
+    order: req.body.order,
+    completed: req.body.completed
+  });
+    todo.save();
+    res.json(Todos);
 });
 
 // GET /api/todos[/id] - get a specific todo item.
 app.get('/api/todos/:id', function(req, res) {
-  var id = Number(req.params.id);
-  var todo = todos.find(function(todoItem) {
-    return todoItem.id === id;
+  var id = req.params.id;
+   Todos.find({_id: id}).then(function(tempTodo) {
+    console.log(tempTodo);
+    res.json(tempTodo);
   });
-  res.json(todo);
 });
 
 // PUT /api/todos[/id] - update a todo item. Returns the modified todo item.
 app.put('/api/todos/:id', function(req, res) {
-  var id = Number(req.params.id);
-  var todo = todos.find(function(todoItem) {
-    return todoItem.id === id;
+  var id = req.params.id;
+  var todo = Todos.find({_id: id}).then(function(todoItem) {
+
+    res.json(todo);
+
   });
-  res.json(todo);
 });
 
 // PATCH /api/todos[/id] - partially update a todo item. Returns the modified todo item.
 app.patch('/api/todos/:id', function(req, res) {
-  var id = Number(req.params.id);
-  var todo = todos.find(function(todoItem) {
+  var id = req.params.id;
+  var todo = Todos.find({_id: id}).then(function(todoItem) {
     return todoItem.id === id;
   });
   res.json(todo);
@@ -60,13 +71,20 @@ app.patch('/api/todos/:id', function(req, res) {
 
 // DELETE /api/todos[/id] - deletes a todo item. Returns the todo item that was deleted.
 app.delete('/api/todos/:id', function(req, res) {
-  var id = Number(req.params.id);
-  var todo = todos.find(function(todoItem) {
+  var id = req.params.id;
+  var todo = Todos.find({_id: id}).then(function(todoItem) {
     return todoItem.id === id;
   });
     res.json(todo);
 });
 
+// const todo = new Todos({
+//   title: 'Finish Assessment'
+//   , order: 1
+//   , completed: false
+// });
+//
+// todo.save();
 
 app.listen(3000, function () {
     console.log('Express running on http://localhost:3000/.');
---
+++ models/todos.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+//calling Schema method in mongoose module
+//defining schema, step 1
+var todosSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    default: ''
+  },
+  order: Number,
+  completed: {
+    type: Boolean,
+    default: false
+  }
+});
+//todos is name of the collection that we give--step 2
+const Todos = mongoose.model('Todos', todosSchema);
+
+module.exports = Todos;
